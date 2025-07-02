e8d3e990f3d87b25aede254915845c2dcd1969e2
fivefootwan <wanvipa.wb@gmail.com>
2025-07-02 15:23:43 +0700

    done with linking aboutpage

---
+++ pixelfed-app/src/AboutPage.js
@@ -0,0 +1,14 @@
+import { useParams } from 'react-router-dom'; // if you need URL params
+import { Link, useNavigate } from 'react-router-dom'; // if you want links or navigation
+
+
+function AboutPage() {
+
+    return (
+        <div>
+        <h1>this is heading</h1>
+        </div>
+    )
+}
+
+export default AboutPage;
--- pixelfed-app/src/App.js
+++ pixelfed-app/src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+import AboutPage from './AboutPage';
+
 
 function App() {
   return (
@@ -7,43 +10,48 @@ function App() {
 
       <header className="App-header">
         <h1>
-          Title Page
+          Social Media Page
         </h1>
 
         <div className='Navigation-Menu'>
-          <p>Homepage</p>
+          <Link to='/'><p>Homepage</p></Link>
           <p>||</p>
-          <p>About Page</p>
+          <Link to='/aboutPage'><p>About Page</p></Link>
         </div>
 
       </header>
 
-      <div className='Content-Body'>
+      <Routes>
+        <Route path='/' element= {
+          <div className='Content-Body'>
 
-        <div className='Cards'>
-          <div className='Card'>
-            <p className='Username'>This is username</p>
-            <img className='Post-Image'src='https://i.pinimg.com/736x/e6/81/51/e68151625fa88075ffa1f206b48bf9c9.jpg'/>
-            <div className='Post-Details'>
-              <p>This is Card</p>
-              <p>|</p>
-              <p>This is Card</p>
-            </div>
-          </div>
+            <div className='Cards'>
+              <div className='Card'>
+                <p className='Username'>This is username</p>
+                <img className='Post-Image'src='https://i.pinimg.com/736x/e6/81/51/e68151625fa88075ffa1f206b48bf9c9.jpg'/>
+                <div className='Post-Details'>
+                  <p>This is Card</p>
+                  <p>|</p>
+                  <p>This is Card</p>
+                </div>
+              </div>
 
-          <div className='Card'>
-            <p className='Username'>This is username</p>
-            <img className='Post-Image'src='https://i.pinimg.com/736x/e6/81/51/e68151625fa88075ffa1f206b48bf9c9.jpg'/>
-            <div className='Post-Details'>
-              <p>This is Card</p>
-              <p>|</p>
-              <p>This is Card</p>
+              <div className='Card'>
+                <p className='Username'>This is username</p>
+                <img className='Post-Image'src='https://i.pinimg.com/736x/e6/81/51/e68151625fa88075ffa1f206b48bf9c9.jpg'/>
+                <div className='Post-Details'>
+                  <p>This is Card</p>
+                  <p>|</p>
+                  <p>This is Card</p>
+                </div>
+              </div>
+
             </div>
-          </div>
 
         </div>
-
-      </div>
+        }/>
+        <Route path='/aboutPage' element={<AboutPage />} />
+      </Routes>
 
     </div>
   );
